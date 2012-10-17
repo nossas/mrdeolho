9a1e04b2e18a90ac61b4963f3978cca17e8bedb2
Luiz Fonseca <runeroniek@gmail.com>
2012-10-17 15:09:58 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrdeolho::Application.config.secret_token = '07161bf0ece83b76ffeb66c095f4faf59e1841f551654fa410b32e14ea5124c0f2203eb9da0451364ed370853d91a7b48cf879e9419c078c03b657e07285a380'
