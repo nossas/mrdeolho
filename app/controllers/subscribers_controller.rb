33f3e80baa2e5f0893852cd11c1c349578c21af0
Luiz Fonseca <runeroniek@gmail.com>
2012-10-22 18:32:11 -0300

    Little improvements on flash messages

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,5 +1,9 @@
+# coding: utf-8
 class SubscribersController < ApplicationController
   inherit_resources
   actions :index, :new, :create
-
+  
+  def create
+    create!(notice: "Seu e-mail foi cadastrado com sucesso! :)")
+  end
 end
