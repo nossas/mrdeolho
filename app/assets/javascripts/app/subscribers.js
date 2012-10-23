b281a1d1b9cf99407f1e2d3558a8f515fb9f832b
Luiz Fonseca <runeroniek@gmail.com>
2012-10-23 12:00:02 -0300

    Try to fix minification issues.

--- app/assets/javascripts/app/subscribers.js
+++ app/assets/javascripts/app/subscribers.js
@@ -1,4 +1,4 @@
-function Subscribers($scope) {
+function Subscribers($scope, $http) {
   
   $scope.video = "http://www.youtube.com/embed/K0XQ1JCf3e0?autoplay=1&wmode=transparent&rel=0"
   
@@ -8,6 +8,9 @@ function Subscribers($scope) {
   }
       
 }
+
+Subscribers.$inject = ['$scope', '$http'];
+
 
 (!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs"));
 (function(d, s, id) {
